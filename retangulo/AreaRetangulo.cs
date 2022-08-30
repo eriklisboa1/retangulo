8c2d8711498452002b19eeffd65b11e7842bc9bd
Erik Lisboa <bonitoerik@gmail.com>
2022-08-30 02:13:14 -0300

    Adicionar arquivos de projeto.

---
+++ retangulo/AreaRetangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace retangulo
+{
+    public class AreaRetangulo
+    {
+       public double Largura;
+       public double Altura;
+
+        public double Area()
+        {
+               return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (2 * Largura) + (2 * Altura);
+        }
+        public double Diagonal()
+        {
+            double cal = (Largura * Largura) + (Altura * Altura);
+            return Math.Sqrt(cal);
+                
+        }
+         
+        public override string ToString()
+        {
+            return "Area = " + Area().ToString("F2", CultureInfo.InvariantCulture)
+               + "\nPerimetro = " + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+               + "\nDiagonal = " + Diagonal();
+        }
+    }
+}
---
+++ retangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AreaRetangulo a = new AreaRetangulo();
+
+            Console.WriteLine("Qual a Largura deste retangulo?");
+            a.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual a altura deste retangulo?");
+            a.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine(a);
+        }
+        
+    }
+}
